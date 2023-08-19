d04779081748283da0bb87ceca40f76bc7fb64fc
patilvijay9400 <patilvijay416940@gmail.com>
2023-08-19 12:22:44 +0530

    testing

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,32 +1,32 @@
 import { Outlet, Link } from "react-router-dom";
 
 const Layout = () => {
-    return (
-        <div className="row mx-0">
-            <div className="col-2 bg-primary vh-100 pt-2">
-                <nav>
-                    <h2 className="text-center text-white">NTech</h2>
-                    <ul className="list-unstyled sidemenu">
-                        <li>
-                            <Link to="/layout">Home</Link>
-                        </li>
-                        <li>
-                            <Link to="user">Users</Link>
-                        </li>
-                        <li>
-                            <Link to="profile">Profile</Link>
-                        </li>
-                        <li>
-                            <Link to="../">Logout</Link>
-                        </li>
-                    </ul>
-                </nav>
-            </div>
-            <div className="col-10 p-3">
-                <Outlet />
-            </div>
-        </div>
-    )
+  return (
+    <div className="row mx-0">
+      <div className="col-2 bg-primary vh-100 pt-2">
+        <nav>
+          <h2 className="text-center text-white">NTech</h2>
+          <ul className="list-unstyled sidemenu">
+            <li>
+              <Link to="/layout">Home</Link>
+            </li>
+            <li>
+              <Link to="user">Users</Link>
+            </li>
+            <li>
+              <Link to="profile">Profile</Link>
+            </li>
+            <li>
+              <Link to="../">Logout</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+      <div className="col-10 p-4">
+        <Outlet />
+      </div>
+    </div>
+  );
 };
 
 export default Layout;
